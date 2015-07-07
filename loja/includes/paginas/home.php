99131ff292103b9674548dcf4461586486dec9eb
rogerior <rogerionrc@gmail.com>
2015-07-07 09:25:46 -0300

    #26 Desenvolvimento do módulo responsável por exibir os produtos em destaque na home, é mostrado aleatoriamente os produtos que possuem a flag: destaque=1

--- loja/includes/paginas/home.php
+++ loja/includes/paginas/home.php
@@ -2,7 +2,7 @@
     
     <h5>Nossos Parceiros</h5>
     <div style="width: 100%; height: 50px; margin-left: auto; margin-right: auto; position: relative; top: 50%; overflow: hidden;">            
-            <div id="logomarcasParceiros" style="position: absolute; height: 50px; width: 150%; left: -110%;">
+            <div id="logomarcasParceiros" style="position: absolute; height: 50px; left: -110%;">
                 <a href="#" style="margin-right: 10px;"> <img src="logomarcas/samsung-logo.jpg" height="50px"> </a>
                 <a href="#" style="margin-right: 10px;"> <img src="logomarcas/motorola-logo.png" height="50px"> </a>
                 <a href="#" style="margin-right: 10px;"> <img src="logomarcas/sony-logo.gif" height="50px"> </a>
@@ -15,4 +15,30 @@
                 <a href="#" style="margin-right: 10px;"> <img src="logomarcas/lg-logo.png" height="50px"> </a>
             </div>
     </div>
+    
+    <div class="col-md-12 categoria">
+    <br><br><h3>Confira nossos produtos em destaque:</h3>
+    <?php
+        $conexao = mysqli_connect("localhost", "root", "", "caiudocaminhao");
+        $sql = "SELECT * FROM produtos WHERE destaque=1;";
+        $produtosDestaque = mysqli_query($conexao, $sql);
+        if($produtosDestaque->num_rows > 0) {
+            $arrayProdutos = array();
+            while($destaque = $produtosDestaque->fetch_array()) {
+                $arrayProdutos[] = $destaque;
+            }
+            $numerosAleatorios = range(0, ($produtosDestaque->num_rows - 1));
+            shuffle($numerosAleatorios);
+            for($i = 0; $i < $produtosDestaque->num_rows; $i++) { ?>
+                <div class="col-md-2 produto" style="background-color: #F0F8FF; margin-right: 4px;">
+                    <a style="text-decoration: none; color: black;" href="index.php?pagina=produto&id=<?php echo $arrayProdutos[$numerosAleatorios[$i]]['id']; ?>">
+                        <h2> <?php echo $arrayProdutos[$numerosAleatorios[$i]]['nome']; ?> </h2>
+                        <img src=" <?php echo $arrayProdutos[$numerosAleatorios[$i]]['foto']; ?> ">
+                        <p> R$<?php echo $arrayProdutos[$numerosAleatorios[$i]]['valor']; ?> </p>
+                    </a>
+                </div>
+            <?php } ?>
+        <?php } ?>
+    </div>
+    
 </div>
