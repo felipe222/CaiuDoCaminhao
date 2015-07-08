f80b677f01cc78ee1792741429a4b1cebb030bae
felipe222 <felipecrispim222@gmail.com>
2015-07-08 00:05:46 -0300

    Update produto.php
    Alterado caminho do botão comprar para o carrinho

--- loja/includes/paginas/produto.php
+++ loja/includes/paginas/produto.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
     // CONEXÃO PARA TESTE
     $conexao = mysqli_connect("localhost", "root", "", "caiudocaminhao");
     $sql = "SELECT * FROM produtos WHERE id = {$_GET['id']}";
@@ -11,7 +11,8 @@
     </div>
     <div class="col-md-6">
         <h1><?php echo $produto['nome'];?></h1>
-        <p>R$ <?php echo $produto['valor'];?><button>Comprar</button></p>
+        <p>R$ <?php echo $produto['valor'];?>
+	<p> <?php echo '<a href="carrinho.php?acao=add&id='.$produto['id'].'">Comprar</a>'?></p>;
         <p><?php echo $produto['descricao'];?></p>
     </div>
 </div>
